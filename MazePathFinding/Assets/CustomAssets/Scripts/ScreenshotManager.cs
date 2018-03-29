64ee62709457858efeaeaeb8b9707cc91630df9d
LeakGamesPC\Max <maxerygin1@gmail.com>
2018-03-29 18:35:13 +0300

    sequence step class

--- MazePathFinding/Assets/CustomAssets/Scripts/ScreenshotManager.cs
+++ MazePathFinding/Assets/CustomAssets/Scripts/ScreenshotManager.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class ScreenshotManager : MonoBehaviour {
 
+    [System.Serializable]
+    public class SequenceStep
+    {
+        public int w = 5;
+        public int h = 5;
+        public Vector3 startPos;
+        public Vector3 endPos;
+        public int maze;
+        public int path;
+    }
+
     static ScreenshotManager _instance;
     public static ScreenshotManager Instance
     {
@@ -13,12 +24,21 @@ public class ScreenshotManager : MonoBehaviour {
         }
     }
 
+    public UIManager uiManager;
+    public Transform startPoint;
+    public Transform endPoint;
+
+    public List<SequenceStep> sequence = new List<SequenceStep>();
+
     void Start () {
         _instance = this;
     }
 	
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SaveSequenceStep();
+        }
 	}
 
     public void AdjustCamera(int w, int h)
@@ -30,5 +50,44 @@ public class ScreenshotManager : MonoBehaviour {
         float vPos = maxDimention * 4;
 
         transform.position = new Vector3(hPos, vPos, hPos);
+    }
+
+    void SaveSequenceStep()
+    {
+        SequenceStep step = new SequenceStep();
+        step.w = int.Parse(uiManager.inputWidth.text);
+        step.h = int.Parse(uiManager.inputHeight.text);
+        step.startPos = startPoint.position;
+        step.endPos = endPoint.position;
+
+        if (uiManager.toggleMaze[0].isOn)
+        {
+            step.maze = 1;
+        }
+        else if (uiManager.toggleMaze[1].isOn)
+        {
+            step.maze = 2;
+
+        }
+        else if (uiManager.toggleMaze[2].isOn)
+        {
+            step.maze = 3;
+        }
+
+        if (uiManager.togglePath[0].isOn)
+        {
+            step.path = 1;
+        }
+        else if (uiManager.togglePath[1].isOn)
+        {
+            step.path = 2;
+
+        }
+        else if (uiManager.togglePath[2].isOn)
+        {
+            step.path = 3;
+        }
+
+        sequence.Add(step);
     }
 }
